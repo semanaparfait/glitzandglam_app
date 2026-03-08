724d4be56df68948c9695998482839a4a405b218
semanaparfait <p.semana@alustudent.com>
2026-03-08 10:03:58 +0200

    folder structuring and adding pages

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,57 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import {  Tabs } from "expo-router";
+import { Feather, Ionicons } from "@expo/vector-icons";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function TabsLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+  <Tabs
+  screenOptions={{
+    headerShown: false,
+    tabBarActiveTintColor: '#907764',
+    tabBarInactiveTintColor: '',
+    tabBarShowLabel:false,
+    tabBarStyle:{
+      backgroundColor: '#fff',
+      borderTopWidth:1,
+      borderTopColor: '#F0F0F0',
+      height: 56,
+      paddingTop:8,
+    }
+  }}
+  >
+    <Tabs.Screen
+      name="index"
+      options={{
+        tabBarIcon: ({color,focused}) => <Ionicons name={focused ? "home" : "home-outline"} size={26} color={color} />,
         headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+        title: "Home",
+      }}
+    />
+
+    <Tabs.Screen
+      name="cart"
+      options={{
+        tabBarIcon: ({color,focused}) => <Feather name={focused ? "shopping-cart" : "shopping-cart"} size={26} color={color} />,
+        headerShown: false,
+        title: "Cart",
+      }}
+    />
+    <Tabs.Screen
+      name="favorities"
+      options={{
+        tabBarIcon: ({color,focused}) => <Ionicons name={focused ? "heart" : "heart-outline"} size={26} color={color} />,
+        headerShown: false,
+        title: "Favorities",
+      }}
+    />
+    <Tabs.Screen
+      name="profile"
+      options={{
+        tabBarIcon: ({color,focused}) => <Ionicons name={focused ? "person" : "person-outline"} size={26} color={color} />,
+        headerShown: false,
+        title: "Profile",
+      }}
+    />
+  </Tabs>
+)
 }
--- app/(tabs)/favorities.tsx
+++ app/(tabs)/favorities.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Home() {
+export default function Favorities() {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Favorities</Text>
     </View>
   )
 }
