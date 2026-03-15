a9229b8fb39717c31907079d676ecfb0e4182b5d
semanaparfait <p.semana@alustudent.com>
2026-03-15 10:49:01 +0200

    finalizing checkout

--- app/checkout.tsx
+++ app/checkout.tsx
@@ -19,6 +19,14 @@ export default function Checkout() {
   } = useAddresses();
   const router = useRouter();
   const { data: cartItems, isLoading, error } = useCartItems();
+  const subtotal = useMemo(
+    () =>
+      cartItems?.items?.reduce(
+        (sum: number, item: any) => sum + item.totalPrice,
+        0,
+      ) ?? 0,
+    [cartItems],
+  );
 
   const addressList = useMemo(() => {
     if (Array.isArray(addresses)) return addresses;
@@ -105,7 +113,74 @@ export default function Checkout() {
             </>
           )}
         </View>
+        {/* ----------------payments---------------- */}
+        <Text className="font-bold text-lg mb-3">Payment Method</Text>
+        <View className="  gap-4 mb-6">
+          <TouchableOpacity
+            onPress={() => setSelectedPaymentMethod("phoneNumber")}
+            className={`bg-white p-4 rounded-xl mb-2 shadow-sm  flex-row items-center  border-2  ${
+              selectedPaymentMethod === "phoneNumber"
+                ? "border-primary "
+                : "border-transparent"
+            }`}
+          >
+            <Ionicons name="phone-portrait" size={20} color="#6b7280" />
+            <View className="ml-2 flex-1">
+              <Text className="font-bold text-base">Phone Number</Text>
+              <Text className="text-sm mt-1">
+                Pay with MOMO or Airtel Money
+              </Text>
+            </View>
+            {selectedPaymentMethod === "phoneNumber" && (
+              <Ionicons name="checkmark-circle" size={24} color="#907764" />
+            )}
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => setSelectedPaymentMethod("cash")}
+            className={`bg-white p-4 rounded-xl mb-4 shadow-sm  flex-row items-center  border-2${
+              selectedPaymentMethod === "cash"
+                ? "border-primary "
+                : "border-transparent "
+            }`}
+          >
+            <Ionicons name="cash-outline" size={20} color="#6b7280" />
+            <View className="ml-2 flex-1">
+              <Text className="font-bold text-base">Cash on Delivery</Text>
+              <Text className="text-sm mt-1">
+                Pay when you receive the package
+              </Text>
+            </View>
+            {selectedPaymentMethod === "cash" && (
+              <Ionicons name="checkmark-circle" size={24} color="#907764" />
+            )}
+          </TouchableOpacity>
+        </View>
       </ScrollView>
+      {/* ---------summary----------- */}
+      <View>
+        <View className="p-4 border-t bg-white shadow-lg border-gray-100">
+          <Text className="text-lg font-bold mb-4">Order Summary</Text>
+          <View className="flex-row justify-between mb-2">
+            <Text className="text-gray-400">subTotal</Text>
+            <Text className="font-bold">{subtotal.toLocaleString()} RWF</Text>
+          </View>
+          <View className="flex-row justify-between mb-2">
+            <Text className="text-gray-400">Tax</Text>
+            <Text className="font-bold">0.00 FRW</Text>
+          </View>
+          <View className="flex-row justify-between mb-2">
+            <Text className="font-bold text-xl">Total</Text>
+            <Text className="font-bold text-xl ">
+              {subtotal.toLocaleString()} RWF
+            </Text>
+          </View>
+          <TouchableOpacity className="bg-primary py-3 rounded-full mt-4">
+            <Text className="text-white text-center font-bold text-lg">
+              Place Order
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
